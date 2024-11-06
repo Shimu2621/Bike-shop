ac27f1efec83bef9861eba32dee621f700334aaa
Shimu Ziasmin <shimu2621@gmail.com>
2024-11-05 23:01:56 -0500

    homeslider done

--- src/components/home/HomeSlider.jsx
+++ src/components/home/HomeSlider.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, FreeMode, Autoplay } from "swiper/modules";
+import sliders from "../../staticData/slider";
+
+import "swiper/css";
+import "swiper/css/pagination";
 
 const HomeSlider = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home_slider main_container">
+      <Swiper
+        pagination={{
+          dynamicBullets: true,
+          clickable: true,
+        }}
+        autoplay={{
+          delay: 3000,
+        }}
+        loop={true}
+        modules={[Pagination, Autoplay, FreeMode]}
+        freeMode={true}
+        className="mySwiper"
+      >
+        {sliders?.map((slider) => {
+          return (
+            <SwiperSlide key={slider?.id} className="slider_slide">
+              <img
+                src={slider?.image}
+                alt={`Slide ${slider.id}`}
+                className="slider_image"
+              />
+              <div className="slider_text">
+                <h2>{slider?.title}</h2>
+                <p>{slider?.text}</p>
+                <button>{slider?.button}</button>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </div>
   );
 };
---
+++ src/staticData/slider.js
@@ -0,0 +1,24 @@
+const sliders = [
+    {
+        id: 1,
+        image: "/images/slider3.png",
+        title: "LOVE EVERY RIDE",
+        text: "Believe in the power of the ride",
+        button: "Shop Now"
+    },
+    {
+        id: 2,
+        image: "/images/slider7.png",
+        title: "ENJOY THE RIDE",
+        text: "The best way to travel is on a bike",
+        button: "Shop Now"
+    },
+    {
+        id: 3,
+        image: "/images/slider8.png",
+        title: "BEST CHOICE FOR BIKES",
+        text: "Be the best version of yourself",
+        button: "Shop Now"
+    },
+]
+export default sliders;
